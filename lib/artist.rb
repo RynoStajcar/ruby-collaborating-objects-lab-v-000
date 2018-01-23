6ec5eb628e1bce152508974b60752874393f8268
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-23 15:40:52 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,7 +40,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == false
+    if self.find_by_name(name)
       self.create_by_name(name)
     end
   end
